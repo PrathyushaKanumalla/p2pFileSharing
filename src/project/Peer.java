69ba8e3cfa2232169b3b86a18b5cb49691f00a52
PrathyushaKanumalla <k.prathusha@gmail.com>
2017-11-23 17:06:08 -0500

    made peer as singleton class

--- src/project/Peer.java
+++ src/project/Peer.java
@@ -2,6 +2,16 @@ package project;
 
 public class Peer {
 	
+	private Peer() {}
+	
+	private static class PeerSingletonHelper {
+		private static final Peer peer = new Peer();
+	}
+	
+	public static Peer getInstance() {
+		return PeerSingletonHelper.peer;
+	}
+	
 	public int peerID;
 	public int numOfPrefferredNeighbors;
 	public int unchokingInterval;
