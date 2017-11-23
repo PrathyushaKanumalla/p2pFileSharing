10bcc4509a9b477da38636ddebb5732363fc9bef
PrathyushaKanumalla <k.prathusha@gmail.com>
2017-11-23 16:37:36 -0500

    removed .class files and modified peer

--- src/project/Peer.java
+++ src/project/Peer.java
@@ -2,16 +2,6 @@ package project;
 
 public class Peer {
 	
-	private Peer() {}
-	
-	private static class SingletonPeerHelper {
-		private static final Peer myPeer = new Peer();
-	}
-	
-	private static Peer getInstance() {
-		return SingletonPeerHelper.myPeer;
-	}
-	
 	public int peerID;
 	public int numOfPrefferredNeighbors;
 	public int unchokingInterval;
