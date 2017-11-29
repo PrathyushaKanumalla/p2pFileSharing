9fa48166065f98f2cc2c8451d09f8228164a542d
sridharreddy7 <sridhar.roger@gmail.com>
2017-11-28 21:20:53 -0500

    simple bug

--- src/project/Server.java
+++ src/project/Server.java
@@ -136,11 +136,11 @@ public class Server extends Thread{
 							send request again by putting state = unchoke;
 							if choke occurs -go to server listen again**/
 							byte[] message = new byte[5];
-							while(in.available()<0){
-								
-							}
+//							while(in.available()<0){
+//								
+//							}
 //							if(in.available() >0){
-							while(in.available()>0){}
+							while(in.available()<0){}
 								in.read(message);
 								if (message[4] == MsgType.PIECE.value) {
 									byte[] reqPieceIndex = new byte[4];
